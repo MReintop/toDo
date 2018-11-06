8e2668783634d280750230ca09307fefed7aa5ad
stasmaj <s.majevski@cointraffic.io>
2018-11-06 22:17:32 +0200

    minor changes

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -4,24 +4,18 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
     model(){
-     return this.store.findAll('task');
+        return this.store.findAll('task');
     },
     actions:{
         toggleDone(task){
-            if(this.get('controller').get('task')){
-                task.get('isdone') ? task.set('isdone', false) : task.set('isdone',true);
-                task.save();
-            }
+            task.toggleProperty('isdone');
         },
         addTask() {
-            if (this.get('controller').get('task')) {
-                this.get('store').createRecord("task", {
-                    id:Date.now(),
-                    title: this.get('controller').get('task'),
-                    isdone: false
-                });
-            }
-            this.set('task', "");
+            this.get('store').createRecord("task", {
+                id:Date.now(),
+                title: this.get('controller').get('task'),
+                isdone: false
+            });
         },
          deleteTodo(task) {
             task.destroyRecord();
