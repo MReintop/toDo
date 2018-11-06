38364d102d3e88e5308b1991f96e226f8f9f9553
stasmaj <s.majevski@cointraffic.io>
2018-11-06 21:38:32 +0200

    Changes: in view and actions

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -4,43 +4,27 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
     model(){
-
      return this.store.findAll('task');
     },
     actions:{
-        toggleDone(taskid){
-            this.store.findRecord('task', taskid).then(function(task) {
-            task.get('isdone');
-                if(task.get('isdone')){
-                    task.set('isdone', false);
-                }else{
-                    task.set('isdone',true);
-                }
-
-        task.save();
-            });
+        toggleDone(task){
+            if(this.get('controller').get('task')){
+                task.get('isdone') ? task.set('isdone', false) : task.set('isdone',true);
+                task.save();
+            }
         },
-        addTask: function(){
-            var task = this.get('controller').get('task');
-            console.log(task);
-            let object1={
-                id:Date.now(),
-                title:task,
-                isdone:false
-            };
-            console.log(object1.title);
-
-         //THIS IS WHERE IT GOES WRONG
-            this.store.createRecord('task',object1);
-
-
+        addTask() {
+            if (this.get('controller').get('task')) {
+                this.get('store').createRecord("task", {
+                    id:Date.now(),
+                    title: this.get('controller').get('task'),
+                    isdone: false
+                });
+            }
+            this.set('task', "");
         },
-         deleteTodo(id) {
-            this.store.findRecord('task', id).then(function (task) {
-                task.deleteRecord();
-                task.get('isDeleted'); // => true
-                task.save(); // => DELETE to /posts/1
-        })}
-
+         deleteTodo(task) {
+            task.destroyRecord();
+        }
 }});
 
