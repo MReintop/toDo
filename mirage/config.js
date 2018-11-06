981b4412a62828b60a74492ea5d38ac8fba391ff
stasmaj <s.majevski@cointraffic.io>
2018-11-06 21:37:54 +0200

    Added: routes in mirage

--- mirage/config.js
+++ mirage/config.js
@@ -1,5 +1,3 @@
-import Mirage from 'ember-cli-mirage';
-
 export default function() {
     this.namespace = '/api';
 
@@ -17,13 +15,13 @@ export default function() {
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
+  
 
 
     this.get('/tasks');
-    this.post('/tasks');
     this.patch('/tasks/:id');
+
+    this.get('/tasks/:id');
+    this.post('/tasks');
     this.del('/tasks/:id');
-
-
 }
